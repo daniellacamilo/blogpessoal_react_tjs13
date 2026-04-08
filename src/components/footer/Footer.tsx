48c5955becd8a3da8b854104e145307a7c7556fa
Daniella Camilo <daniella.camilos@gmail.com>
2026-04-08 20:50:18 -0300

    Adição das rotas no React

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,13 +9,19 @@ export function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                             <p className='text-x1 font-bold'>
-                                Blog Pessoal Generation | Copyright : {data}
+                                Blog Pessoal - Desenvolvido por Daniella Camilo  | Copyright : {data}
                             </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <p className='text-lg'>Acesse aqui as minhas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/daniella-camilo-3b64b0393/" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/@daniellacamilo/" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/daniella.camilo.3/" target="_blank">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>                  
                 </div>
             </div>
